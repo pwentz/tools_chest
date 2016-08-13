f22ed900de4ad90525cbb12a409b484d82f8e8f0
pwentz <patrick.w.wentz@gmail.com>
2016-08-12 19:04:26 -0600

    finish edit and delete functionality using react + ajax, consider looking into CSRF authenticity warning.

--- app/assets/javascripts/_all_tools.js.jsx
+++ app/assets/javascripts/_all_tools.js.jsx
@@ -1,10 +1,17 @@
 var AllTools = React.createClass({
+  onUpdate(newTool) {
+    this.props.handleEdit(newTool);
+  },
+  
   render() {
     var tools = this.props.tools.map((tool, index) => {
       return (
         <div key={tool.id}>
-          <h4>{++index}: {tool.name}</h4>
-          <p><strong>Price:</strong> {tool.price}</p>
+          <Tool tool={tool}
+                index={++index}
+                handleDelete={this.props.handleDelete.bind(null, tool)}
+                handleEdit={this.onUpdate}
+              />
         </div>
       )
     })
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -12,11 +12,52 @@ var Body = React.createClass({
     this.setState({ tools: newTools })
   },
 
+  handleDelete(tool) {
+    var newTools = this.state.tools.filter((t) => {
+      return t.id !== tool.id 
+    });
+
+    $.ajax({
+      url: `/api/v1/admin/tools/${tool.id}`,
+      type: 'DELETE',
+      success: () => {
+        console.log(`Successfully deleted ${tool.name}`);
+      }
+    });
+
+    this.setState({ tools: newTools });
+  },
+
+  onUpdate(newTool) {
+    $.ajax({
+      url: `/api/v1/admin/tools/${newTool.id}`,
+      type: 'PUT',
+      data: { tool: newTool },
+      success: () => {
+        console.log(`Successfully updated ${newTool.name}`)
+        this.swapTools(newTool);
+      }
+    });
+  },
+
+  swapTools(newTool) {
+    var newTools = this.state.tools.map((t) => {
+      if (t.id === newTool.id) {
+        return newTool;
+      }
+      else {
+        return t;
+      }
+    })
+
+    this.setState({ tools: newTools });
+  },
+
   render() {
     return (
       <div>
-        <NewTool handleSubmit={this.handleSubmit} />
-        <AllTools tools={this.state.tools} />
+        <NewTool handleSubmit={this.handleSubmit}  />
+        <AllTools tools={this.state.tools} handleDelete={this.handleDelete} handleEdit={this.onUpdate} />
       </div>
     )
   }
